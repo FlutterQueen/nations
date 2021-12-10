5cce107e0f4593e7c0d7ac732cafc03433716be6
Ahmed Masoud <maxzod66@gmail.com>
2021-12-10 14:40:47 +0200

    remove unused import

--- lib/src/extensions/nations_base.dart
+++ lib/src/extensions/nations_base.dart
@@ -1,6 +1,5 @@
 import 'dart:ui' show window;
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:nations/nations.dart';
